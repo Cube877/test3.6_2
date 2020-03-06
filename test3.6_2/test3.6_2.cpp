7160bfd4a5321503f91240ae5d78110f544be44b
Cube <877211593@qq.com>
2020-03-06 14:39:21 +0800

    无

--- test3.6_2/test3.6_2.cpp
+++ test3.6_2/test3.6_2.cpp
@@ -2,10 +2,40 @@
 //
 
 #include "stdafx.h"
+#include <opencv.hpp>
+#include <iostream>
 
+using namespace cv;
+using namespace std;
 
 int main()
 {
+	VideoCapture cap;
+	cap.open(0);
+
+	if (!cap.isOpened())
+	{
+		std::cout << "不能打开视频文件" << std::endl;
+		return-1;
+	}
+	
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "不能读帧" << std::endl;
+			break;
+		}
+		else
+		{
+			cv::imshow("Frame", frame);
+		}
+		waitKey(30);
+	}
     return 0;
 }
 
